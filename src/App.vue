64f4457c90b06d09e648a5295d517d4ed6aeedba
Bluice Zhen <bluice.zhen@gmail.com>
2019-12-11 22:51:01 +0800

    add a beatiful upload button.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
+    <input id="id-upload" type="file" accept="image/*" v-on:change="clickUploadFile($event.target.files)"/>
     <TopBar/>
+    <Main/>
   </div>
 </template>
 
 <script>
-import TopBar from './components/TopBar.vue'
+import TopBar from './components/TopBar.vue';
+import Main from './components/Main.vue'
 
 export default {
   name: 'app',
-  components: {
-    TopBar
-  }
+  methods: {
+    clickUploadFile(fileList) {
+      if (fileList.length != 0) {
+        let file = fileList[0];
+        console.log(file);
+      }
+    }
+  },
+  components: { TopBar, Main }
 }
 </script>
 
@@ -21,6 +30,11 @@ body {
   padding: 0;
 }
 #app {
+  flex-direction: column;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  display: flex;
+}
+#id-upload {
+  display: none;
 }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="main">
+    <el-button v-on:click="buttonOnClick()">Upload</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Mian",
+  methods: {
+    buttonOnClick: function() {
+      document.getElementById('id-upload').click()
+    }
+  }
+};
+</script>
+
+<style>
+.main {
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-grow: 1;
+  height: 100vh;
+}
+</style>
